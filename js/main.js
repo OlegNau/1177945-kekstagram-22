39180eef7ce3cd660eb66530b65a62368d0f6a0b
OPNAUMOV <asler-72@mail.ru>
2021-02-20 00:49:36 +0300

    исправил

--- js/main.js
+++ js/main.js
@@ -6,10 +6,6 @@ const getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomValue();
-
-//Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
 let maxLength = 1;
 let str = 'abvgde';
 
@@ -26,33 +22,29 @@ const COMMENTS  = ['Всё отлично!',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 const quantityComments = {
-  MIN: 0,
-  MAX: 3
-};
+  MIN: 1,
+  MAX: 3};
 
 const LIKES = {
   MIN: 15,
-  MAX: 200
-};
+  MAX: 200};
 
 const PHOTOINFO = ['Мы с колбасятиной едем путешествовать',
-'Кот Пельмень',
-'Катюха пилит Леху',
-'Кто не работает- тот ест',
-'Пытаюсь понять почему люди не хотят быть грачами',
-'Пылесос засосал хомяка'
-];
+  'Кот Пельмень',
+  'Катюха пилит Леху',
+  'Кто не работает- тот ест',
+  'Пытаюсь понять почему люди не хотят быть грачами',
+  'Пылесос засосал хомяка'];
 
 const NAMES = ['Ганс',
-'Герман',
-'Адольф',
-'Урсула',
-'Вольфганг',
-'Рудольф'];
+  'Герман',
+  'Адольф',
+  'Урсула',
+  'Вольфганг',
+  'Рудольф'];
 
 const SIMILAR_PHOTOINFO_COUNT = 25;
 
@@ -62,29 +54,28 @@ const getRandomArrayElement = (elements) => {
 
 const createDescriptionPhoto = () => {
   let newDescriptions = [];
-    for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++) {
     newDescriptions[i] = {
-    id: i+1,
-    url: 'photos/' + (i + 1) + '.jpg',
-    description: getRandomArrayElement(PHOTOINFO),
-    likes: getRandomValue(LIKES.MIN,LIKES.MAX),
-    comments: createComments(quantityComments.MIN,quantityComments.MAX)
-    };
+      id: i+1,
+      url: 'photos/' + (i + 1) + '.jpg',
+      description: getRandomArrayElement(PHOTOINFO),
+      likes: getRandomValue(LIKES.MIN,LIKES.MAX),
+      comments: createComments(getRandomValue(quantityComments.MIN,quantityComments.MAX))};
   }
   return newDescriptions
 };
 
-const createComments = () => {
-   let newComments = [];
-   for (let i = quantityComments.MIN; i < 3; i++) {
-    newComments[i] = {
-    id: i+1,
-    avatar: 'img/avatar-' + (getRandomValue(1, 6)) + '.svg',
-    message: getRandomArrayElement(COMMENTS),
-    name: getRandomArrayElement(NAMES)
-    };
-   }
-   return newComments
+const createComments = (commentsCount) => {
+  let newComment = [];
+  for (let i = commentsCount; i < 4; i++) {
+    newComment[i] = {
+      id: i+1,
+      avatar: 'img/avatar-' + (getRandomValue(1, 6)) + '.svg',
+      message: getRandomArrayElement(COMMENTS),
+      name: getRandomArrayElement(NAMES)};
+  }
+  return newComment;
 };
 
+// eslint-disable-next-line
 console.log(createDescriptionPhoto(SIMILAR_PHOTOINFO_COUNT));
